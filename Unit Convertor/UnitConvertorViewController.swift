ae56584392703de12630200cd23bd90fdd8e401b
Lakshan Thilakarathne <lakshan@nipuns-macbook-pro.local>
2018-08-17 20:23:52 +0530

    Added the edit changed event to all the text fields in weight tab

--- Unit Convertor/UnitConvertorViewController.swift
+++ Unit Convertor/UnitConvertorViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class UnitConvertorViewController: UIViewController {
 
+    @IBOutlet weak var txtGram: UITextField!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -19,6 +22,21 @@ class UnitConvertorViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func txtFieldEditingChanged(_ sender: UITextField, forEvent event: UIEvent) {
+        switch sender.tag {
+        case 1...4:
+            print("click on tag 1...4")
+        case 5...9:
+            print("click on tag 5...9")
+        case 10...14:
+            print("click on tag 10...14")
+        case 15...19:
+            print("click on tag 15...19")
+        default:
+            print("Invalid tag")
+        }
     }
     
     func clearTextFields(){
